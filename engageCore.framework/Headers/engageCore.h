6efeb3a7eb9493a84a91a4ab73f289f1e8aa2a04
Sudeep George <sudeep.geoge@diagnal.com>
2017-08-07 19:16:06 +0530

    EngageSDK Initial commit : 1.Segments 2.Campaigns 3.Session Management 4.Push Notification Campaigns

---
+++ engageCore.framework/Headers/engageCore.h
@@ -0,0 +1,19 @@
+//
+//  engageCore.h
+//  engageCore
+//
+//  Created by Sudeep George on 13/07/17.
+//  Copyright © 2017 DIAGNAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for engageCore.
+FOUNDATION_EXPORT double engageCoreVersionNumber;
+
+//! Project version string for engageCore.
+FOUNDATION_EXPORT const unsigned char engageCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <engageCore/PublicHeader.h>
+
+
---
+++ engageCore.podspec
@@ -0,0 +1,14 @@
+Pod::Spec.new do |s|
+  s.name = 'engageCore'
+  s.version = '0.0.1'
+  s.author = 'Diagnal'
+  s.license = {:type => 'Commercial'}
+  s.homepage = 'https://github.com/diagnal/engage-ios-sdk'
+  s.summary = 'The EngageCore iOS SDK for App Personalization and Engagement.'
+  
+  s.source = { :git => 'https://github.com/diagnal/engage-ios-sdk.git' , :tag => s.version.to_s }
+  s.vendored_frameworks = 'engageCore.framework'
+  s.platform     = :ios
+  s.requires_arc = true
+  s.ios.deployment_target = "8.0"
+end
