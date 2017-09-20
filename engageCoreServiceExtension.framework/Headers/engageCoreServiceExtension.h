fdfb5ffcad9e019a5eb4abd253d6aba422589773
Sudeep George <sudeep.geoge@diagnal.com>
2017-09-20 14:20:33 +0530

    KE-539 : Engage service extension SDK.

---
+++ engageCoreServiceExtension.framework/Headers/engageCoreServiceExtension.h
@@ -0,0 +1,19 @@
+//
+//  engageCoreServiceExtension.h
+//  engageCoreServiceExtension
+//
+//  Created by Sudeep George on 06/09/17.
+//  Copyright © 2017 Diagnal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for engageCoreServiceExtension.
+FOUNDATION_EXPORT double engageCoreServiceExtensionVersionNumber;
+
+//! Project version string for engageCoreServiceExtension.
+FOUNDATION_EXPORT const unsigned char engageCoreServiceExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <engageCoreServiceExtension/PublicHeader.h>
+
+
