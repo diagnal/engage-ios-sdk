8b04500037e4c88c8e268bcb791177f6ac7fb6aa
Sudeep George <sudeep.geoge@diagnal.com>
2017-10-04 13:31:01 +0530

    Helper Classes update to swift 3

--- Helper Classes/EngageFIRAnalytics.swift
+++ Helper Classes/EngageFIRAnalytics.swift
@@ -14,9 +14,9 @@ class EngageFIRAnalytics: NSObject {
     
     /// Logs an app event.
     class func logEventWithName (name : String , parameters : [String : AnyObject]?) {
-        FIRAnalytics.logEventWithName(name, parameters: parameters)
+        Analytics.logEvent(name, parameters: parameters)
         if parameters != nil {
-            Engage.track(name, params: parameters!)
+            Engage.track(eventName: name, params: parameters!)
         }
     }
     
@@ -24,41 +24,41 @@ class EngageFIRAnalytics: NSObject {
     /// user property values persist throughout the app lifecycle and across sessions.
     
     class func setUserPropertyString(value : String , forName name : String) {
-        FIRAnalytics.setUserPropertyString(value, forName: name)
+        Analytics.setUserProperty(value, forName: name)
     }
     
     /// Sets the user ID property.
     class func setUserID(userID : String?) {
-        FIRAnalytics.setUserID(userID)
+        Analytics.setUserID(userID)
     }
     
     /// Sets the current screen name, which specifies the current visual context in your app. This helps
     /// identify the areas in your app where users spend their time and how they interact with your app.
     
     class func setScreenName(screenName : String? , screenClass screenClassOverride:String?) {
-        FIRAnalytics.setScreenName(screenName, screenClass: screenClassOverride)
+        Analytics.setScreenName(screenName, screenClass: screenClassOverride)
     }
     
     /// The unique ID for this instance of the application.
     class func appInstanceID() -> String {
-        return FIRAnalytics.appInstanceID()
+        return Analytics.appInstanceID()
     }
     
     /// Handles events related to a URL session that are waiting to be processed.
-    class func handleEventsForBackgroundURLSession(identifier : String , completionHandler : ((Void) -> Void)) {
-        FIRAnalytics.handleEventsForBackgroundURLSession(identifier, completionHandler: completionHandler)
+    class func handleEventsForBackgroundURLSession(identifier : String , completionHandler : @escaping ((Void) -> Void)) {
+        Analytics.handleEvents(forBackgroundURLSession: identifier, completionHandler: completionHandler)
     }
     
     /// Handles the event when the app is launched by a URL.
-    class func handleOpenURL(url : NSURL) {
-        FIRAnalytics.handleOpenURL(url)
+    class func handleOpenURL(url : URL) {
+        Analytics.handleOpen(url)
     }
     
     /**
      * Handles the event when the app receives data associated with user activity that includes a
      * Universal Link (on iOS 9.0 and above).
      */
-    class func handleUserActivity(userActivity : AnyObject) {
-        FIRAnalytics.handleUserActivity(userActivity)
+    class func handleUserActivity(userActivity : Any) {
+        Analytics.handleUserActivity(userActivity)
     }
 }
